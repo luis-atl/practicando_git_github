b4e56aefc176184ea83ca532588e9e933cbd5a37
Luis Trejo <luis_trejo_lara@msn.com>
2023-05-20 19:44:24 -0600

    Se elimino el archivo byegit.php

--- byegit.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Adios Git";
-
-echo "Haciendo cambios en algun archivo antes de comprometerlos pero que esta en stage";
-
-?>
--- hellogit.php
+++ hellogit.php
@@ -1,7 +1,7 @@
 <?php
 echo "Hello Git!";
 
-
+$var1 = "Probando cambios con archivos ya comprometidos";
 
 
 ?>
